e00e4a787292357b0695e18171f00de5941348a7
esphere api <e.sphere.project@gmail.com>
2025-08-26 11:40:04 +0100

    mise a jour

--- esphere-ass-eureka-server/esphere-ass-eureka-server/src/test/java/zuul_server/EurekaServerApplicationTests.java
+++ esphere-ass-eureka-server/esphere-ass-eureka-server/src/test/java/zuul_server/EurekaServerApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
       
-class ZuulServerApplicationTests {
+class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
