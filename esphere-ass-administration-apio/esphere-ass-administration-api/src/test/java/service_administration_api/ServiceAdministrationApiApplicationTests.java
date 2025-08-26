99d4ce272cf45ccd3bb527c2f006b90f5cd0c8f9
esphere api <e.sphere.project@gmail.com>
2025-08-26 12:03:59 +0100

    résolution conflit bootstrap.properties

--- esphere-ass-administration-apio/esphere-ass-administration-api/src/test/java/service_administration_api/ServiceAdministrationApiApplicationTests.java
+++ esphere-ass-administration-apio/esphere-ass-administration-api/src/test/java/service_administration_api/ServiceAdministrationApiApplicationTests.java
@@ -1,13 +1,12 @@
 package service_administration_api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiceAdministrationApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+    
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
