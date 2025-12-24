688703584ecd26b83bd415f0fc37aec0067501c7
FRANK-DEV-JAVA <jiatou.frank@zenitheinsurance.com>
2025-12-24 12:11:31 +0100

    Ajout du dossier biometrie depuis la branche frank

---
+++ biometrie/biometry/src/test/java/db/biometry/biometry/BiometryApplicationTests.java
@@ -0,0 +1,16 @@
+package db.biometry.biometry;
+
+
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometryApplicationTests {
+
+    @IgnoreForBinding
+	@Test
+	void contextLoads() {
+	}
+
+}
