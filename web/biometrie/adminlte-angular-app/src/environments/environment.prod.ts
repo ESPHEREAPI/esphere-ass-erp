ccbd87b13cc8ca19cca303dcc4e36d218d4cc782
FRANK-DEV-JAVA <jiatou.frank@zenitheinsurance.com>
2026-01-05 12:56:57 +0100

    suppresion dossier web

--- web/biometrie/adminlte-angular-app/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-
-  production: true,
-  //apiUrl:'http://109.228.49.89:9001',
- // apiUrl:'https://localhost:8080',
-   apiUrl:'',
-  token_key:'3cfa76ef890d4aed2d3981a7c93bd1a13c8796dafcb4f94fa578234a0df56b321'
-
-};
