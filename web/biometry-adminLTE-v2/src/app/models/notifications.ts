8911a01cd477b9c5ecba37d4ca460797783b6c41
FRANK-DEV-JAVA <jiatou.frank@zenitheinsurance.com>
2026-01-05 12:44:48 +0100

    suppression du dossier web

--- web/biometry-adminLTE-v2/src/app/models/notifications.ts
+++
@@ -1,5 +0,0 @@
-export interface Notifications {
-    consultations: number;
-  ordonnances: number;
-  examens: number;
-}
--- web/biometry-adminLTE-v2/src/app/models/statistiques-mensuelles.ts
+++
@@ -1,9 +0,0 @@
-export interface StatistiquesMensuelles {
-     mois: string;
-  consultations: number;
-  ordonnances: number;
-  examens: number;
-  montantConsultations?: number;
-  montantOrdonnances?: number;
-  montantExamens?: number;
-}
